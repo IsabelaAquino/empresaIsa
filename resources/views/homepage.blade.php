dee4a2b44157b884211351cd23e93c297fc2c0ff
Isabela <isabela.isavzp@gmail.com>
2019-04-14 20:11:28 -0300

    commit referente a trabalho 1 da disciplina de web II

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Empresa da Isabela</h1>
+    <h2>
+        Desenvolvimento web || Entre em contato:
+    </h2> 
+    <form action="contato" method="get">
+            <input type="submit" value="Ir para página de Contato!" />
+    </form>
+    <br>
+    <form action="login" method="get">
+            <input type="submit" value="Ir para página de Login!" />
+    </form>
+</body>
+</html>
